e7b63bd503d877586466a4a073d8d95a5410a0e0
jackakers13 <jackakers13@gmail.com>
2020-11-08 08:28:41 -0600

    Rename player_pos to player_minimap_pos

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-var player_pos;
+var player_minimap_pos;
 var player_inv = new Object();
 
 function init_inv() {
@@ -11,50 +11,50 @@ function init_inv() {
 }
 
 function add_current_color_to_inv() {
-	player_inv[minimap[player_pos.x][player_pos.y]] = true;
+	player_inv[minimap[player_minimap_pos.x][player_minimap_pos.y]] = true;
 }
 
 function go_up() {
-  if(player_pos.y - 1 < 0)
+  if(player_minimap_pos.y - 1 < 0)
     return;
-  if(minimap[player_pos.x][player_pos.y - 1] == 0)
+  if(minimap[player_minimap_pos.x][player_minimap_pos.y - 1] == 0)
     return;
   draw_level();  
   add_current_color_to_inv();
-  player_pos.y -= 1;
+  player_minimap_pos.y -= 1;
 }
 
 function go_down() {
-  if(player_pos.y + 1 >= minimap_size)
+  if(player_minimap_pos.y + 1 >= minimap_size)
     return;
-  if(minimap[player_pos.x][player_pos.y + 1] == 0)
+  if(minimap[player_minimap_pos.x][player_minimap_pos.y + 1] == 0)
     return;
   draw_level();
   add_current_color_to_inv();
-  player_pos.y += 1;
+  player_minimap_pos.y += 1;
 }
 
 function go_left() {
-  if(player_pos.x - 1 < 0)
+  if(player_minimap_pos.x - 1 < 0)
     return;
-  if(minimap[player_pos.x - 1][player_pos.y] == 0)
+  if(minimap[player_minimap_pos.x - 1][player_minimap_pos.y] == 0)
     return;
   draw_level();
   add_current_color_to_inv();
-  player_pos.x -= 1;
+  player_minimap_pos.x -= 1;
 }
 
 function go_right() {
-  if(player_pos.x + 1 >= minimap_size)
+  if(player_minimap_pos.x + 1 >= minimap_size)
     return;
-  if(minimap[player_pos.x + 1][player_pos.y] == 0)
+  if(minimap[player_minimap_pos.x + 1][player_minimap_pos.y] == 0)
     return;
   draw_level();
   add_current_color_to_inv();
-  player_pos.x += 1;
+  player_minimap_pos.x += 1;
 }
 
 function die() {
-  player_pos.x = minimap_starting_room_obj.x;
-  player_pos.y = minimap_starting_room_obj.y;
+  player_minimap_pos.x = minimap_starting_room_obj.x;
+  player_minimap_pos.y = minimap_starting_room_obj.y;
 }
