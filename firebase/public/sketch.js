b0c30425d0a910236d43d4315f4b22b3c3709b46
sawgzq <sawgzq@mail.missouri.edu>
2020-11-07 21:40:02 -0600

    upload to website

---
+++ firebase/public/sketch.js
@@ -0,0 +1,44 @@
+import { generate_minimap } from '/gui.js';
+
+document.addEventListener("DOMContentLoaded", event=>{
+	const app = firebase.app();	
+});
+
+fps = 2**5;
+
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+  frameRate(fps);
+  
+  generate_minimap();
+  input_setup();
+  setGamestateMainMenu();
+  set_button_positions();
+
+}
+
+function windowResized() {
+
+  resizeCanvas(windowWidth, windowHeight);
+
+  set_button_positions();
+
+}
+
+function draw() {
+
+  clear();
+
+  background(128*Math.sin(frameCount/fps) + 128, 128*Math.sin(frameCount/fps + 64) + 128, 128*Math.sin(frameCount/fps + 128) + 128);
+
+  draw_level();
+  draw_menu();
+  draw_minimap();
+  draw_progress_bar();
+
+}
+
+function keyPressed() {
+	on_key_pressed();
+}
